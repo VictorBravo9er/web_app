0a417e9c2351977d6f75d226dd7ff5b624fde1af
Vikramjit Borah <vikramjitborah@gmail.com>
2024-06-13 10:58:13 +0530

    we

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function alterMe(){
+    alert("hi")
+}
+
+document.querySelector("button").onclick = () => {
+    console.log("button clicked");
+    const content = "a random number created " + Math.floor(Math.random()*1000);
+    let prevEl = document.querySelector("#txtEl") 
+    if (prevEl){
+        prevEl.textContent = content;
+        return
+    }
+    const newElem = document.createElement('p');
+    newElem.textContent = content;
+    newElem.id = "txtEl";
+    document.body.appendChild(newElem)
+}
+
+// document.querySelector("button").onmouseover
+
+// document.querySelector("button").addEventListener("click", (this, ev) => {});
